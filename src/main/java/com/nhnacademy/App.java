34f5c6e2730c3ea9a4579fc8a3eb61f2533acfef
nhn-academy-marco <marco@ nhnacademy.com>
2024-04-06 11:55:18 +0900

    multi-thread 추가

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -1,14 +1,22 @@
 package com.nhnacademy;
 
+import com.nhnacademy.http.HttpRequestHandler;
 import com.nhnacademy.http.SimpleHttpServer;
 import lombok.extern.slf4j.Slf4j;
 import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 @Slf4j
 public class App 
 {
     public static void main( String[] args ) throws IOException {
-        SimpleHttpServer simpleHttpServer = new SimpleHttpServer(8080);
-        simpleHttpServer.start();
+        try(ServerSocket serverSocket = new ServerSocket(8080);){
+            while(true){
+                Socket client = serverSocket.accept();
+                Thread thread = new Thread(new HttpRequestHandler(client));
+                thread.start();
+            }
+        }
     }
 }
---
+++ src/main/java/com/nhnacademy/http/HttpRequestHandler.java
@@ -0,0 +1,67 @@
+package com.nhnacademy.http;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Objects;
+
+@Slf4j
+public class HttpRequestHandler implements Runnable {
+    private final Socket client;
+
+    public HttpRequestHandler(Socket client) {
+        this.client = client;
+    }
+
+
+    @Override
+    public void run() {
+        StringBuilder requestBuilder = new StringBuilder();
+
+        try(BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+        ) {
+
+            while (true) {
+                String line = bufferedReader.readLine();
+                //todo StringBuilder에 append
+                requestBuilder.append(line);
+                log.debug("line:{}", line);
+                if (Objects.isNull(line) || line.length() == 0) {
+                    //todo 종료 조건 null or size==0
+                    break;
+                }
+            }
+            //todo RequestBuilder에 append된 데이터를 parcing 하여 HttpRequest가 동작할 수 있도록 구현합니다.
+
+            StringBuilder responseBody = new StringBuilder();
+            responseBody.append("<html>");
+            responseBody.append("<body>");
+            responseBody.append("<h1>hello java</h1>");
+            responseBody.append("</body>");
+            responseBody.append("</html>");
+
+            StringBuilder responseHeader = new StringBuilder();
+
+            responseHeader.append(String.format("HTTP/1.0 200 OK%s",System.lineSeparator()));
+            responseHeader.append(String.format("Server: HTTP server/0.1%s",System.lineSeparator()));
+            responseHeader.append(String.format("Content-type: text/html; charset=%s%s","UTF-8",System.lineSeparator()));
+            responseHeader.append(String.format("Connection: Closed%s",System.lineSeparator()));
+            responseHeader.append(String.format("Content-Length:%d %s%s",responseBody.length(),System.lineSeparator(),System.lineSeparator()));
+
+            bufferedWriter.write(responseHeader.toString());
+            bufferedWriter.write(responseBody.toString());
+            bufferedWriter.flush();
+            client.close();
+        }catch (IOException e){
+            if (Objects.nonNull(client)){
+                try {
+                    client.close();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        }
+    }
+}
