44ad87279ebc027ddd936fadc377a2074a4fa8f8
nhn-academy-marco <marco@ nhnacademy.com>
2024-04-12 17:46:10 +0900

    copyright 추가

--- src/main/java/com/nhnacademy/http/context/ContextHolder.java
+++ src/main/java/com/nhnacademy/http/context/ContextHolder.java
@@ -1,3 +1,15 @@
+/*
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ * + Copyright 2024. NHN Academy Corp. All rights reserved.
+ * + * While every precaution has been taken in the preparation of this resource,  assumes no
+ * + responsibility for errors or omissions, or for damages resulting from the use of the information
+ * + contained herein
+ * + No part of this resource may be reproduced, stored in a retrieval system, or transmitted, in any
+ * + form or by any means, electronic, mechanical, photocopying, recording, or otherwise, without the
+ * + prior written permission.
+ * +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+ */
+
 package com.nhnacademy.http.context;
 
 //TODO#3 - Context에 접근할 수 있도록 ContextHolder를 Singleton 구현 합니다.
