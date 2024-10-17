98ba2058e1f6b781dcea082f841dd01597859d3d
uuiskk <hus101405@naver.com>
2024-10-17 17:42:57 +0900

    complete

--- src/main/java/com/nhnacademy/http/SimpleHttpServer.java
+++ src/main/java/com/nhnacademy/http/SimpleHttpServer.java
@@ -59,9 +59,9 @@ public class SimpleHttpServer {
         */
         Context context = ContextHolder.getApplicationContext();
         context.setAttribute("/index.html", new IndexHttpService());
-        context.setAttribute("/info.html", new IndexHttpService());
-        context.setAttribute("/404.html", new IndexHttpService());
-        context.setAttribute("/405.html", new IndexHttpService());
+        context.setAttribute("/info.html", new InfoHttpService());
+        context.setAttribute("/404.html", new NotFoundHttpService());
+        context.setAttribute("/405.html", new MethodNotAllowedService());
 
         /*TODO#5 Counter 구현을 위해서 CounterUtils.CONTEXT_COUNTER_NAME 으로, 0l 을 context에 등록 합니다.
          */
